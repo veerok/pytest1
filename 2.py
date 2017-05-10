e412907378caa44ec85744fd27deaf74ffffd7b8
Вера <vselivanova4@gmail.com>
2017-05-10 10:36:06 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,11 @@
 
+a = int(input())
+
+if a == 1 or a == 4 or a == 5 or a == 8 or a == 9 or a == 12:
+  print('весна')
+elif a == 2 or a == 6 or a == 10:
+  print('лето')
+elif a == 3 or a == 7 or a == 11:
+  print('зима')
+else :
+  print(0)
